83a5f9da61308c745267a3c85da24c019a7fea78
Julien Maupetit <julien@tailordev.fr>
2016-02-12 16:14:42 +0100

    Add base express app

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-var menubar = require('menubar')
+var menubar = require('menubar');
 
-var mb = menubar()
+var mb = menubar({
+    'dir': 'app',
+    'tooltip': 'Watson',
+    'height:': 200
+});
 
 mb.on('ready', function ready () {
-  console.log('app is ready')
-})
+    console.log('app is ready');
+
+    var exec = require('child_process').exec;
+
+    function puts(error, stdout, stderr) { console.log(stdout) }
+
+    exec("watson status", puts);
+});
